0bbdc41b11bc2ba9d2802a5499915d978044cb88
scpark <hongtaemin919@gmail.com>
2026-04-08 17:19:06 +0900

    jpaRepository

--- src/main/java/me/scpark/springdeveloper/repository/BlogRepository.java
+++ src/main/java/me/scpark/springdeveloper/repository/BlogRepository.java
@@ -1,8 +1,11 @@
 package me.scpark.springdeveloper.repository;
 
 import me.scpark.springdeveloper.dao.Article;
+import org.springframework.data.jpa.repository.JpaRepository; // 추가
+import org.springframework.stereotype.Repository; // 추가
 
-public interface BlogRepository {
-
-    Article save(Article entity);
+@Repository // 스프링에게 이 인터페이스가 리포지토리임을 알려줍니다.
+public interface BlogRepository extends JpaRepository<Article, Long> {
+    // 이제 save() 같은 메서드를 직접 만들지 않아도 JpaRepository가 제공해줍니다.
+    // 기존에 작성하신 Article save(Article entity); 는 삭제하셔도 무방합니다.
 }
